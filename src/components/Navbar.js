bc2f729f64ff226661aee0eb59b6084d7dff5621
CarlEverest78 <thepythoneer@gmail.com>
2022-08-05 19:03:01 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+// import icon from './React-icon.png'
+
+function Navbar() {
+    return(
+        <div class="navbar">
+            {/* <img src={icon} alt="react logo" class="logo"/> */}
+            <h1>ReactFacts</h1>
+            <p id="subText">React Course - Project 1</p>
+        </div>
+
+    )
+}
+
+export default Navbar
